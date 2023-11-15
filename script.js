ee261754ca2e0fab5419ea318b10d3fcf76149d5
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-15 16:43:46 +0100

    collegati input e cardboard su js per startare il programma

---
+++ script.js
@@ -0,0 +1,13 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// genera numero random tra 1 e 6 (quindi no 0 e 6+1)
+// genera un secondo numero randomico per il computer
+// if per estrarre il vincitore in base al numero piu alto uscito
+
+// 1 input collegato con id
+const startButton = document.getElementById('startButton');
+// 2 collegare cardBoard 
+let cardBoard = document.getElementById('cardBoard');

77f9def90077d7b7ac2a0c4db16bb13f866f1fa7
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-15 23:54:59 +0100

    lista mail autorizzate + inizio funzione per verifica email

--- mail/script.js
+++ mail/script.js
@@ -4,3 +4,21 @@
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+// lista mail autorizzate (array)
+const authorizedEmails = [
+    'berna@lol.com',
+    'gino@lol.com',
+    'marcellino@lol.com',
+    'ginello@lol.com',
+    'pino@lol.com',
+    'matteo@lol.com',
+    'valeria@lol.com',
+    'franci@lol.com',
+    'ricca@lol.com',
+    'michele@lol.com',
+];
+
+let function verifyEmail() {
+    const
+}

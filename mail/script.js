fc3b4f7f3e60cda88a96894aae8b03e6c7f71700
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-15 23:26:16 +0100

    struttura html + css + js per Mail

---
+++ mail/script.js
@@ -0,0 +1,6 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
+// stampa un messaggio appropriato sull’esito del controllo.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
